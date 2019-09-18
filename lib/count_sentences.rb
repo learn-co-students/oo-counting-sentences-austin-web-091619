a958dde000820d0b16721f0bd7016dcdbf944a00
Joellehelm <joellenhelm@gmail.com>
2019-09-18 16:56:46 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
-require 'pry'
-
+require "pry"
 class String
 
   def sentence?
-    
+  self.end_with?(".")
+  
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+   num = 0
+   words = self.split(/[.?!]/)
+   words.each do |sent|
+   if sent == ""
+      num += 1
+   end
+   
+    end
+    words.count - num
+   end
 
-  end
+
 end
+
